95297cdc9cd1d5162ecc1d3e4e36d3ef88a8d376
chenyanky <91323733+chenyanky@users.noreply.github.com>
2023-06-28 00:46:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-// $("#btn").on("click",getFolder);
-function init(){
-    var btn=document.getElementById("btn");
-    btn.onclick=getFolder;  
-}
-function getFolder(){
-    console.log("clicked");
-    const fs = require('fs');
-    const path = require('path');
 
-    // 文件夹路径
-    const folderPath = '/path/to/folder';
+// $("#btn").on("click",selectFolder);
 
-    // 遍历目录下的文件
-    fs.readdir(folderPath, (err, files) => {
-        if (err) throw err;
+// function selectFolder(){
 
-        // 循环遍历文件
-        files.forEach((file) => {
-            // 获取文件完整路径
-            const filePath = path.join(folderPath, file);
+//     const input = document.createElement('input');
+//     input.type = 'file';
+//     input.webkitdirectory = true; // 添加webkitdirectory属性，弹出文件夹选择器
+//     input.onchange = function(event) {
 
-            // 判断文件类型是否为pdf或doc/docx
-            if (/\.(pdf|doc|docx)$/i.test(filePath)) {
-            // 执行相应的操作，比如复制或移动文件
-            console.log('找到匹配的文件：', filePath);
-            }
-        });
-    });
-}
-init();
+//         const folderPath = event.target.files[0].path; // 获取选中的文件夹路径
+//         console.log(folderPath); // 在控制台中输出文件夹路径，您可以将其发送到服务器进行处理
+//         $("#path").text(folderPath);
+//     };
+//     input.click(); // 模拟点击input元素，弹出文件夹选择器
+// }
+const form = document.querySelector('form');
+form.addEventListener('submit', async (event) => {
+	event.preventDefault();
+	const data = {
+		folder_path : document.getElementById('folder_path').files[0].path
+	};
+	const response = await fetch('/folder', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	});
+	const result = await response.json();
+	if (response.ok) {
+		alert('成功选择文件夹：' + result.folder_path);
+	} else {
+		alert('错误：' + result.message);
+	}
+});
