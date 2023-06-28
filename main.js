2ac58c97e9d18489515a17487996ebb09e23a22c
chenyanky <91323733+chenyanky@users.noreply.github.com>
2023-06-29 00:57:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,64 @@
+// const form = document.querySelector('form');
 
-// $("#btn").on("click",selectFolder);
+// form.addEventListener('submit', async (event) => {
+// 	event.preventDefault();
+// 	const data = {
+// 		folder_path : document.getElementById('folder_path').files[0].path
+// 	};
+// 	const response = await fetch('/folder', {
+// 		method: 'POST',
+// 		headers: {
+// 			'Content-Type': 'application/json'
+// 		},
+// 		body: JSON.stringify(data)
+// 	});
+// 	const result = await response.json();
+// 	if (response.ok) {
+// 		alert('成功选择文件夹：' + result.folder_path);
+// 	} else {
+// 		alert('错误：' + result.message);
+// 	}
+// });
+$("#openFileDialog").on("click",openFileDialog);
+function openFileDialog() {
+    $.ajax({
+		type:'POST',
+		url: '/get_folder_path/',  // 后端接口的URL
+		// method: 'GET',
+		success: function(data) {
+			// var path=JSON.parse(data)
+            console.log(data.folder_path);  // 处理从服务器返回的数据
+			$("#path").val(data.folder_path);
+		}
+	  });
+  }
 
-// function selectFolder(){
+$("#seek").on("click",seekKeyword);
+function seekKeyword(){
 
-//     const input = document.createElement('input');
-//     input.type = 'file';
-//     input.webkitdirectory = true; // 添加webkitdirectory属性，弹出文件夹选择器
-//     input.onchange = function(event) {
+	var keyword=$("#keyword").val();
+	var path=$("#path").val();
 
-//         const folderPath = event.target.files[0].path; // 获取选中的文件夹路径
-//         console.log(folderPath); // 在控制台中输出文件夹路径，您可以将其发送到服务器进行处理
-//         $("#path").text(folderPath);
-//     };
-//     input.click(); // 模拟点击input元素，弹出文件夹选择器
-// }
-const form = document.querySelector('form');
-form.addEventListener('submit', async (event) => {
-	event.preventDefault();
-	const data = {
-		folder_path : document.getElementById('folder_path').files[0].path
-	};
-	const response = await fetch('/folder', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	});
-	const result = await response.json();
-	if (response.ok) {
-		alert('成功选择文件夹：' + result.folder_path);
-	} else {
-		alert('错误：' + result.message);
+	if(path==""){
+		alert("请先选择文件夹！");
 	}
-});
+	else if(keyword==""){
+		alert("请先输入关键词！");
+	}
+	else{
+		$.ajax({
+			// type:'POST',
+			url: '/seek_keyword/', 
+			method:'POST',
+			//将路径和关键词传给后端
+			dataType: 'json',
+			data:{
+				'keyword':keyword,
+				'folder_path':path
+			},
+			success: function(data) {
+				console.log(data.code); 
+			}
+		  });
+	}
+}
